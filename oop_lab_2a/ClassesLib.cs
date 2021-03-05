10c9dbf35f5fe3f9cbd912cee774da651cd7ba67
MaksymKurny <78872568+MaksymKurny@users.noreply.github.com>
2021-03-05 16:25:51 +0200

    Add files via upload

---
+++ oop_lab_2a/ClassesLib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace oop_lab_2a
+{
+    public class CPU
+    {
+        public string Number, Segment;
+        public int Core, Year;
+        public double BFrequency, TFrequency;
+        public bool HyperT;
+
+        public int NumberOfThreads
+        {
+            get
+            {
+                return GetNumberOfThreads();
+            }
+        }
+        public int GetNumberOfThreads()
+        {
+            if (HyperT == true) return (Core * 2);
+            else return (Core);
+        }
+    }
+}
